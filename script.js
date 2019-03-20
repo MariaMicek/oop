5ace7b03d1d8d49873833189d3c7893537d5f42f
Maria Micek <mmicek16@wp.pl>
2019-03-20 11:51:48 +0100

    .call() and .apply()

--- script.js
+++ script.js
@@ -1,65 +1,25 @@
-//first example
+function move(burnt) {
+    this.fuelTank = this.fuelTank - burnt
 
-function makeSound(){
-    console.log(this.sound)
+    if (this.fuelTank < 0) this.fuelTank = 0
 }
 
-console.log(makeSound())
-
-const cat1 = {
-    sound: 'Meow!',
-    makeSound: makeSound,
+const car1 = {
+    fuelTank: 100,
+    move: move,
 }
 
-console.log(cat1.makeSound())
-console.log(makeSound === cat1.makeSound)
-
-const cat2 = {
-    sound: 'Meow! Meow!',
-    makeSound: makeSound,
+const car2 = {
+    fuelTank: 150,
+    move: move,
 }
 
-console.log(cat2.makeSound())
+car1.move(10)
+car1.move(20)
 
-    
-//second example
+car2.move.call(car1, 10)        //gdy funkcja ma więcej parametów to należy podać więcej argumentów 
+car2.move.apply(car1, [20])     //działa tak samo
+car2.move(10)
 
-console.log(this);
-
-function that() {
-    console.log(this);
-}
-
-that();
-
-const me = {
-    name: 'Marysia',
-    showMe: that,
-}
-
-me.showMe();
-
-
-//third example
-
-function removeLeg(){
-    this.legs = this.legs - 1;
-    if (this.legs < 0) {this.legs = 0}
-}
-
-function removeLegs(numberOfLegs){
-    this.legs = this.legs - numberOfLegs;
-    if (this.legs < 0) {this.legs = 0}
-}
-
-const chair = {
-    legs: 4,
-    removeLeg: removeLeg,
-    removeLegs: removeLegs,
-}
-
-chair.removeLeg();
-chair.removeLeg();
-chair.removeLegs(3);
-
-console.log(chair.legs)
+console.log('car1: ', car1)
+console.log('car2: ', car2)
