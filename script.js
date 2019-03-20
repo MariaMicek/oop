c2c0261ce4e01f8b4fa9c524c0b9c86c08a130bf
Maria Micek <mmicek16@wp.pl>
2019-03-20 13:25:16 +0100

    creating 100 cars from one function

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-function move(burnt) {
-    this.fuelTank = this.fuelTank - burnt
+// uwtórz 100 samochodów z różnym paliwem
 
-    if (this.fuelTank < 0) this.fuelTank = 0
+// for (i = 0; i < 100; i++){
+//     cars.push({
+//         fuelTank: 100 - i,
+//     })
+// }
+
+const makeCar = function(fuelTank) {
+    return{
+        fuelTank: fuelTank,
+        move: function (combustion){
+            this.fuelTank = this.fuelTank - combustion
+        }
+    }
 }
 
-const car1 = {
-    fuelTank: 100,
-    move: move,
+const cars = [];
+
+for (i = 0; i < 100; i++){
+    const car = makeCar(100 - i)
+    cars.push(car)
 }
 
-const car2 = {
-    fuelTank: 150,
-    move: move.bind(car1),      //przypisuje funkcje z car1 do car2 (spina je ze sobą), czyli wywołuje ją na car1 a nie na car2
-}
+console.log(cars)
 
-car1.move(10)
-car1.move(20)
+const car10 = cars[9]
+car10.move(10)
 
-car2.move(10)
-car2.move(20)
-car2.move(5)
-
-
-console.log('car1: ', car1)
-console.log('car2: ', car2)
+console.log('car10: ', car10)
