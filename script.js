d0c7f78dfae9554e8481941b463a55bb2be7678b
Maria Micek <mmicek16@wp.pl>
2019-03-21 14:52:48 +0100

    adding elements of array and taking average with constructor function and render function

--- script.js
+++ script.js
@@ -1,12 +1,91 @@
-const ArrayPusher = function() {
+const ArrayPusher = function(selector) {
 
+    this.container = document.querySelector(selector) || document.body
     this.array = []
+    this.inputValue = 0
+    this.render()
 
 }
 
-ArrayPusher.prototype.push = function(x) {
+ArrayPusher.prototype.render = function (){
 
-    this.array.push(x)      // this.array = this.array.concat(x) 
+    this.container.innerHTML = ''
+    const input = document.createElement('input')
+    const button = document.createElement('button')
+    const numbers = document.createElement('p')
+    const arrayContainer = this.renderNumbersArray()
+    const sum = document.createElement('p')
+    const average = document.createElement('p')
+
+    this.container.appendChild(input)
+    this.container.appendChild(button)
+    this.container.appendChild(numbers)
+    this.container.appendChild(arrayContainer)
+    this.container.appendChild(sum)
+    this.container.appendChild(average)
+
+    input.setAttribute('type', 'number')
+    input.value = this.inputValue
+    button.innerText = "ADD"
+    numbers.textContent = `Numbers: `
+    sum.textContent = `Sum: ${this.sum()}`
+    average.textContent = `Average: ${this.average()}`
+
+
+    input.addEventListener(
+
+        'input',
+        event => this.inputValue = Number(event.target.value)
+
+    )
+
+    button.addEventListener(
+
+        'click',
+        () => this.push(this.inputValue)
+
+    )
+
+}
+
+ArrayPusher.prototype.renderNumbersArray = function() {
+
+    const arrayContainer = document.createElement('div')
+
+    this.array.forEach( (number, index) => {
+
+        const div = document.createElement('div')
+        const buttonDelete = document.createElement('button')
+
+        div.innerText = number
+        buttonDelete.innerText = "DELETE"
+
+        buttonDelete.addEventListener(
+            'click',
+            () => this.remove(index)
+        )
+
+        div.appendChild(buttonDelete)
+        arrayContainer.appendChild(div)
+
+    })
+
+    return arrayContainer
+}
+
+ArrayPusher.prototype.push = function(newValue) {
+
+    this.array = this.array.concat(newValue) 
+    
+    this.render()
+
+}
+
+ArrayPusher.prototype.remove = function(removedElementIndex) {
+
+    this.array = this.array.filter((el,i) => i !== removedElementIndex)
+
+    this.render()
 
 }
 
@@ -21,23 +100,9 @@ ArrayPusher.prototype.sum = function() {
 
 ArrayPusher.prototype.average = function() {
     
+    if (this.array.length === 0) return 0
     return this.sum() / this.array.length
 
 }
 
-const pusher1 = new ArrayPusher()
-pusher1.push(1)
-pusher1.push(4)
-pusher1.push(5)
-console.log(pusher1)
-console.log(pusher1.sum())
-console.log(pusher1.average())
-
-const pusher2 = new ArrayPusher()
-pusher2.push(10)
-pusher2.push(14)
-pusher2.push(6)
-console.log(pusher2)
-console.log(pusher2.sum())
-console.log(pusher2.average())
 
