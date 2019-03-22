0872936708e06aacb70ee272222ccc875fb591ff
Maria Micek <mmicek16@wp.pl>
2019-03-22 13:33:11 +0100

    creating objects with classes and inheritance

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-class Counter {
+class Animal {
 
-    constructor(){
-        this.number = 0
+    constructor(legs, sound){
+        this.legs = legs
+        this.sound = sound
     }
 
-    inc(){
-        this.number = this.number + 1
+    makeSound(){
+        console.log(this.sound)
     }
 
-    minus(){
-        this.number = this.number - 1
+    sayHello(){
+        if (this.name){
+            console.log(`Hello, I am ${this.name}`)
+        } else {
+            console.log('I don\'t have a name...')
+        }
+    }
+
+}
+const animal1 = new Animal(8, 'Wrrrr')
+console.log(animal1)
+console.log(animal1.makeSound())
+console.log(animal1.sayHello())
+
+
+
+class Dog extends Animal {              //dziedziczenie
+
+    constructor(name){
+        super(4,'Woof!')             // super() wywołuje constructor klasy nadrzędnej
+        this.name = name
+    }
+
+}
+const dog1 = new Dog('Puszek')
+console.log(dog1)
+console.log(dog1.makeSound())
+console.log(dog1.sayHello())
+
+
+
+class Cat extends Animal {
+
+    constructor(name){
+        super(4, 'Meow...')
+        this.name = name
     }
 }
+const cat1 = new Cat('Mruczek')
+console.log(cat1)
+console.log(cat1.makeSound())
+console.log(cat1.sayHello())
 
-const counter1 = new Counter()
-console.log(counter1)
 
-counter1.inc()
-counter1.inc()
-counter1.inc()
-console.log(counter1)
 
-counter1.minus()
-counter1.minus()
-console.log(counter1)
+
